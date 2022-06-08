855f4a9aa0d83c14f1d104a2ea40ee6918dd3722
dongyeon94 <dongyeon94@naver.com>
2022-06-09 03:14:33 +0900

    timezoneset

--- src/main/java/com/root/App.java
+++ src/main/java/com/root/App.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 //@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class App {
+	@PostConstruct
+	public void timezoneSet(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
